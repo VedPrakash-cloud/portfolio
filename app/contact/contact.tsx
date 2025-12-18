04934297af887c89c3218caedaa7d521aabc1ce2
VedPrakash-cloud <rocky007ved@gmail.com>
2025-12-18 15:12:06 +0530

    social media links added

--- app/contact/contact.tsx
+++ app/contact/contact.tsx
@@ -1,6 +1,8 @@
 import { faGithub, faInstagram, faLinkedinIn } from "@fortawesome/free-brands-svg-icons";
 import { faPaperPlane, faPhoneVolume } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { NextApiRequest, NextApiResponse } from "next";
+// import nodemailer from "nodemailer";
 
 export default function Contact(){
     return (
@@ -20,21 +22,21 @@ export default function Contact(){
                             <p>+91 9910505104</p>
                         </div>
                         <div className="text-2xl text-[#ababab] mb-10">
-                            <a href="#"><FontAwesomeIcon icon={faInstagram} className="mr-5 hover:text-[#ff004f] duration-600 ease-in-out"/></a>
-                            <a href="#"><FontAwesomeIcon icon={faLinkedinIn} className="mr-5 hover:text-[#ff004f] duration-600 ease-in-out"/></a>
-                            <a href="#"><FontAwesomeIcon icon={faGithub} className="hover:text-[#ff004f] duration-600 ease-in-out"/></a>
+                            <a href="https://www.instagram.com/ved_prakash_007/"target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faInstagram} className="mr-5 hover:text-[#ff004f] duration-600 ease-in-out"/></a>
+                            <a href="https://www.linkedin.com/in/ved-prakash-chaubey-563b44142" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faLinkedinIn} className="mr-5 hover:text-[#ff004f] duration-600 ease-in-out"/></a>
+                            <a href="https://github.com/VedPrakash-cloud" target="_blank" rel="noopener noreferrer"><FontAwesomeIcon icon={faGithub} className="hover:text-[#ff004f] duration-600 ease-in-out"/></a>
                         </div>
                         <a href="/Ved_prakash_1764755435311.pdf" download className="border border-[#ff004f] py-3 px-10 rounded-lg hover:bg-[#ff004f] duration-900 ease-in-out cursor-pointer hover:shadow-lg shadow-[#ff004f]/50">Download CV</a>
                     </div>
                     <div className="contact-right">
-                        <form className="">
+                        <form>
                             <input type="text" placeholder="your name" name="Name" required 
                             className="border border-[#262626] rounded-sm p-2 w-full bg-[#262626] outline-none mb-5" />
                             <input type="email" name="email" placeholder="your email" required
                             className="border border-[#262626] rounded-sm p-2 mb-5 w-full bg-[#262626] outline-none" />
                             <textarea name="message" rows={5} placeholder="your message" required
                             className="border border-[#262626] rounded-sm p-2 mb-5 w-full bg-[#262626] outline-none"></textarea>
-                            <button 
+                            <button type="submit"
                             className="py-2 px-7 font-semibold rounded-lg border border-[#ff004f] hover:bg-[#ff004f] duration-900 ease-in-out cursor-pointer hover:shadow-lg shadow-[#ff004f]/50">
                                 Submit
                             </button>
